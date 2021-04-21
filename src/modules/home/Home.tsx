2f24d7643426581af14d31fec29d4f8d194e24cb
giannina <giannecchini.a@gmail.com>
2021-04-21 01:10:01 -0400

    feat: add select register

--- src/modules/home/Home.tsx
+++ src/modules/home/Home.tsx
@@ -11,21 +11,62 @@ const Home = () => {
   const [rut, setRut] = useState("");
   const [salary, setSalary] = useState("");
   const [plans, setPlans] = useState(null);
+  const [error, setError] = useState(false);
 
   // Fetch get all plans
   const getAllPlans = async () => {
-    const response = await fetch(`${API_URI}/plan`);
-    if (!response.ok) {
-      const message = `An error has occured: ${response.status}`;
-      setPlans(message);
-      throw new Error(message);
+    try {
+      const response = await fetch(`${API_URI}/plan`);
+      if (!response.ok) {
+        const message = `An error has occured: ${response.status}`;
+        setPlans(message);
+        throw new Error(message);
+      }
+      const plansResponse = await response.json();
+      setPlans(plansResponse.data);
+    } catch (error) {
+      console.log("error", error);
+      setError(true);
     }
-    const plansResponse = await response.json();
-    setPlans(plansResponse.data);
   };
 
-  const onSubmit = () => {
-    console.log("onSubmit");
+  const onSubmitSearch = async () => {
+    try {
+      const response = await fetch(`${API_URI}/plan/${salary}`);
+      if (!response.ok) {
+        const message = `An error has occured: ${response.status}`;
+        setPlans(message);
+        throw new Error(message);
+      }
+      const plansResponse = await response.json();
+      setPlans(plansResponse.data);
+    } catch (error) {
+      console.log("error", error);
+      setError(true);
+    }
+  };
+
+  const handleSelect = async (plan: string) => {
+    try {
+      const data = { rut: rut, plan: plan };
+      const variables = {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(data),
+      };
+
+      const response = await fetch(`${API_URI}/user`, variables);
+      if (!response.ok) {
+        const message = `An error has occured: ${response.status}`;
+        console.log("error", message);
+        throw new Error(message);
+      }
+      const isPost = await response.json();
+      console.log(isPost);
+    } catch (error) {
+      console.log("error", error);
+      setError(true);
+    }
   };
 
   useEffect(() => {
@@ -35,41 +76,45 @@ const Home = () => {
   return (
     <>
       <Header />
-      <section className={cx(css.cns_content)}>
-        <div
-          className={cx(
-            css.cns_form,
-            "container-row row--between row_align--center"
-          )}
-        >
-          <input
-            value={rut}
-            className={cx(css.cns_form__input, "col_3")}
-            type="text"
-            placeholder="13.234.023-k"
-            onChange={(event) => {
-              setRut(event.target.value);
-            }}
-          />
-          <input
-            value={salary}
-            className={cx(css.cns_form__input, "col_3")}
-            type="text"
-            placeholder="1000000"
-            onChange={(event) => {
-              setSalary(event.target.value);
-            }}
-          />
-          <button
-            onClick={onSubmit}
-            className={cx(css.cns_form__button, "col_3")}
-            type="submit"
-          >
-            buscar
-          </button>
-        </div>
-      </section>
-      <ListPlans list={plans} />
+      {!error && (
+        <>
+          <section className={cx(css.cns_content)}>
+            <div
+              className={cx(
+                css.cns_form,
+                "container-row row--between row_align--center"
+              )}
+            >
+              <input
+                value={rut}
+                className={cx(css.cns_form__input, "col_3")}
+                type="text"
+                placeholder="13.234.023-k"
+                onChange={(event) => {
+                  setRut(event.target.value);
+                }}
+              />
+              <input
+                value={salary}
+                className={cx(css.cns_form__input, "col_3")}
+                type="text"
+                placeholder="1000000"
+                onChange={(event) => {
+                  setSalary(event.target.value);
+                }}
+              />
+              <button
+                onClick={onSubmitSearch}
+                className={cx(css.cns_form__button, "col_3")}
+                type="submit"
+              >
+                buscar
+              </button>
+            </div>
+          </section>
+          <ListPlans list={plans} onSelect={handleSelect} />
+        </>
+      )}
     </>
   );
 };
--- src/modules/home/ListPlans.tsx
+++ src/modules/home/ListPlans.tsx
@@ -3,21 +3,23 @@ import cx from "classnames";
 import css from "./listPlans.module.scss";
 
 const ListPlans = (props) => {
-  const { list } = props;
+  const { list, onSelect } = props;
   if (!list) return null;
 
   return (
     <section className={cx(css.list_content)}>
       {list.map((item: any) => (
         <div
+          key={item.code}
+          onClick={() => onSelect(item.code)}
           className={cx(
             css.itemPlan,
             "container-row row--between row_align--center"
           )}
         >
-          <div>{item?.code}</div>
-          <div>{item?.name}</div>
-          <div>{item?.price}</div>
+          <div>{item.code}</div>
+          <div>{item.name}</div>
+          <div>{item.price}</div>
         </div>
       ))}
     </section>
